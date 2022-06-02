260cf96f7f2d6f641bbe99286b146ecf66c5190d
SeaSkyThe <marceloer2011@gmail.com>
2022-06-02 12:17:48 -0300

    Update and Delete operations added to Expenses

--- expenses/views.py
+++ expenses/views.py
@@ -57,16 +57,50 @@ def add_expense(request):
 
 
 def edit_expense(request, id):
+	categories=Category.objects.all()
 	expense = Expense.objects.get(pk=id)
 	context = {
-		'expense':expense
+		'values': expense,
+		'categories': categories,
 	}
 	
 	if(request.method == 'GET'):
-		
-		return render(request, 'expenses/edit_expense.html', context)
-
-	else:
 		messages.info(request, 'Edit your expense')
-
 		return render(request, 'expenses/edit_expense.html', context)
+
+	if(request.method == 'POST'):
+
+		price = request.POST['price']
+		description = request.POST['description']
+		spending_date = request.POST['spending_date']
+		category = request.POST['category']
+
+		if(not price):
+			messages.error(request, 'Price is a required field')
+			return render(request, 'expenses/edit_expense.html', context)
+
+		if(not description):
+			messages.error(request, 'Description is a required field')
+			return render(request, 'expenses/edit_expense.html', context)
+
+		if(not spending_date):
+			messages.error(request, 'Date is a required field')
+			return render(request, 'expenses/edit_expense.html', context)
+		
+		
+		expense.price = price
+		expense.description = description
+		expense.category = category
+		expense.date = spending_date
+		expense.owner = request.user
+		expense.save()
+
+		messages.success(request, 'Expense updated succesfully')
+
+		return redirect('expenses')
+
+def delete_expense(request, id):
+	expense = Expense.objects.get(pk=id)
+	expense.delete()
+	messages.success(request, 'Expense deleted succesfully')
+	return redirect('expenses')
