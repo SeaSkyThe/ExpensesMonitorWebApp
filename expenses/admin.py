6021a82267128304f099b77db37244ba9c0873c8
SeaSkyThe <marceloer2011@gmail.com>
2022-06-07 22:09:17 -0300

    Changes done in admin page

--- expenses/admin.py
+++ expenses/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Expense, Category
 # Register your models here.
 
-admin.site.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('price', 'description', 'category', 'date')
+    search_field = ('description', 'category', 'date')
+
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
